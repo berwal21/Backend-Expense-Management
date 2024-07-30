05ca2cfb49f0a8e5c34f9adafae4f920baf0f344
berwal21 <amanberwal51@gmail.com>
2024-07-30 15:08:27 +0530

    Backend of Expense Management completed

---
+++ models/transactionModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+    userid:{
+        type: String,
+        required:[true, 'userid is required']
+    },
+    amount:{
+        type: Number,
+        required:[true, 'amount is required']
+    },
+
+    type:{
+        type: String,
+        required:[true, 'type is required']
+    },
+
+    category:{
+        type: String,
+        required:[true, 'category is required']
+    },
+
+    reference:{
+        type: String
+    },
+
+    discription:{
+        type: String,
+        required:[true, 'discription is required']
+    },
+
+    date:{
+        type: Date,
+        required:[true, 'date is required']
+    },
+},{timestamps:true});
+
+const transactionModel = mongoose.model('transaction',transactionSchema);
+
+module.exports = transactionModel;
